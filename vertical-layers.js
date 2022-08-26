5de09c841d07c88ae78ffbae2b37822db15b834c
slashrawr <brandon.michael.lewis@gmail.com>
2022-08-27 01:04:36 +0800

    Added textured brush with Perlin noise and soft edges.

--- vertical-layers.js
+++ vertical-layers.js
@@ -1,3 +1,7 @@
+let show_clouds = true;
+let textured_buildings = true;
+let soft_edges = true;
+
 let min_width = 20;
 let max_width = 50;
 let min_height = 100;
@@ -8,7 +12,7 @@ let palette = {};
 let points = [];
 let groups = [];
 let avgheights = [];
-let segments = 3;
+
 
 function preload() {
   palettes = loadJSON('assets/palettes.json');
@@ -16,23 +20,26 @@ function preload() {
 
 function setup() {
   createCanvas(700, 700);
+  colorMode(RGB);
   max_height = height*0.8;
   noLoop();
   strokeCap(SQUARE);
+  noiseDetail(2,0.65);
   
   
   groups[0] = 0;
   groups[1] = width/3;
   groups[2] = (width/3)*2;
+  groups[3] = (width);
   groups.push(width);
-  console.debug(groups);
+
   
   palette = palettes[int(random(0,199))];
 }
 
 function draw() {
-  let bgcol = random(palette);
-  background(hexToRGB(bgcol,1.0));
+  let bgcol = hexToRGB(random(palette), 255);
+  background(bgcol);
   points = [];
   
   for (let l = 0; l < layers; l++) {
@@ -58,36 +65,56 @@ function draw() {
         agg += h;
         counter++;
       }
-
-      strokeWeight(stroke_width);
-      let col = color(0);
       
-      do {
-        col = random(palette);
-        colour = hexToRGB(col,random(0.3,0.6));
-        stroke(colour);
-      } while (col == bgcol)
+      let col = hexToRGB(random(palette), random(100,180));
       
+      if (textured_buildings) {
+        strokeWeight(1);
+        for (let x = current_width; x < current_width + stroke_width; x++)
+        {
+          for (let y = height; y > (soft_edges ? h*random(0.6,1.0) : h); y--)
+          {
+            let alpha = random(100,180);
+            let n = noise(x, y);
+            alpha *= n;
+            col.setAlpha(alpha);
+            stroke(col);
+            point(x,y);
+          }
+        }
+      }
+      else {
+        strokeWeight(stroke_width);
+        do {
+          let alpha = random(100,180);
+          let n = noise(stroke_width, h);
+          alpha *= n;
+          col = hexToRGB(random(palette), alpha);
+          stroke(col);
+        } while (col == bgcol)
+      }
       line(current_width, height, current_width, h);
       points.push(new p5.Vector(current_width, h));
       current_width += stroke_width;
     }
-    
-  strokeWeight(3);
-  fill(0,0,0,0);
-  bezier(0, avgheights[0]/random(1.5,2), width*0.33, avgheights[1]-random(avgheights[1]), width*0.66, avgheights[2]-random(avgheights[2]), width, avgheights[3]/random(1.5,2));
   
-  avgheights = [];
+  if (show_clouds) {
+    strokeWeight(2);
+    fill(0,0,0,0);
+    bezier(0, avgheights[0]/random(1.5,2), width*0.33, avgheights[1]-random(avgheights[1]), width*0.66, avgheights[2]-random(avgheights[2]), width, avgheights[3]/random(1.5,2));
+    avgheights = [];
+  }
+  
   }
 }
 
-function hexToRGB(h,alpha) {
-  let r = 0, g = 0, b = 0;
-  r = "0x" + h[1] + h[2];
-  g = "0x" + h[3] + h[4];
-  b = "0x" + h[5] + h[6];
-  r = +(r / 255 * 100).toFixed(1);
-  g = +(g / 255 * 100).toFixed(1);
-  b = +(b / 255 * 100).toFixed(1);
-  return "rgba(" + r + "%," + g + "%," + b + "%," + str(alpha) + ")";
+function hexToRGB(hexColour, alpha) {
+  var aRgbHex = hexColour.replace("#","").match(/.{1,2}/g);
+  var aRgb = [
+            parseInt(aRgbHex[0], 16),
+            parseInt(aRgbHex[1], 16),
+            parseInt(aRgbHex[2], 16)
+        ];
+  let colour = color(aRgb[0], aRgb[1], aRgb[2], alpha);
+  return colour;
 }
