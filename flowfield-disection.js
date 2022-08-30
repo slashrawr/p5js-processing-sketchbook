6aef4461afccc108ab33fac334130b0bab82d40d
slashrawr <brandon.michael.lewis@gmail.com>
2022-08-30 14:23:34 +0800

    Added lines and colours.

--- flowfield-disection.js
+++ flowfield-disection.js
@@ -1,25 +1,37 @@
 let showBlocks = false;
-let showGrid = true;
-let showFlowDirection = true;
+let showGrid = false;
+let showFlowDirection = false;
+
+let seed = 134536;
+let animationlength = 5;
 
 let isize = 20;
-
 let _scale = 40;
 let _rows = 0;
 let _cols = 0;
-let _maxspeed = 10;
-let flowfield = [];
+let _maxspeed = 4;
+let _flowfield = [];
 let _particlecount = 500;
 let particles = [];
+let palettes = [];
+let palette = {};
+
+function preload() {
+  palettes = loadJSON('assets/palettes.json');
+}
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 700);
+  randomSeed(seed);
+  noiseSeed(seed);
   colorMode(RGB);
   angleMode(RADIANS);
+  strokeCap(ROUND);
+  palette = palettes[int(random(0,199))];
   rows = floor(height / _scale)
   cols = floor(width / _scale);
-  frameRate(10);
-  noiseDetail(10,0.5);
+  frameRate(30);
+  noiseDetail(4,0.5);
   
   createFlowField();
   for (let i = 0; i < _particlecount; i++) {
@@ -30,12 +42,12 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  let counter = 0;
-   for (let y = 0; y <= rows; y++) {
-  for (let x = 0; x <= cols; x++) {
+  //background(0);
+  //let counter = 0;
    
-      let direction = flowfield[x + y * _scale].heading();
+  for (let x = 0; x <= cols; x++) {
+   for (let y = 0; y <= rows; y++) {
+      let direction = _flowfield[x + y * _scale].heading();
       
       //console.debug(direction);
       
@@ -58,7 +70,7 @@ function draw() {
       if (showFlowDirection) {
         push();
         strokeWeight(2);
-        stroke(255*flowfield[x + y * _scale].mag(),0,0);
+        stroke(255,0,0);
         let x2 = cos(direction)* isize + scaledx;
         let y2 = sin(direction)* isize + scaledy;
         line(scaledx,scaledy,x2,y2);
@@ -67,41 +79,41 @@ function draw() {
     }
   }
   
-  //if (keyIsPressed === true) {
-  
   push()
-  stroke(255);
-  strokeWeight(5);
+  stroke(255, 80);
+  strokeWeight(2);
 
   particles.forEach(particle => {
     let x = floor(particle.position.x / _scale);
     let y = floor(particle.position.y / _scale);
 
-    flow = flowfield[x + y * _scale];
-    //console.debug("x: " + x + " - y:" + y + " - flow:" + flow);
+    flow = _flowfield[x + y * _scale];
+    particle.previousposition = particle.position.copy();
     particle.acceleration.add(flow);
-    //console.debug("acc: " + particle.acceleration + " - vel: " + particle.velocity);
     particle.velocity.add(particle.acceleration);
     particle.velocity.limit(_maxspeed);
     particle.position.add(particle.velocity);
     particle.acceleration.mult(0);
     edgeCheck(particle);
+    stroke(particle.colour);
     point(particle.position.x, particle.position.y);
+    line(particle.previousposition.x, particle.previousposition.y, particle.position.x, particle.position.y);
   })
   
   pop();
- // }
+  
+  if (frameCount >= 30*animationlength)
+    noLoop();
 }
 
 function createFlowField() {
   let counter = 0;
-  for (let y = 0; y <= rows; y++) {
+  
   for (let x = 0; x <= cols; x++) {
-    
+    for (let y = 0; y <= rows; y++) {
       let dir = noise(x/(_scale/2),y/(_scale/2));
       let vec = p5.Vector.fromAngle(TWO_PI*dir, random(0.1,1));
-      flowfield[x + y * _scale] = vec;
-      //console.debug(flowfield[x+y*_scale].heading());     
+      _flowfield[x + y * _scale] = vec;  
     }
   }
 }
@@ -111,19 +123,40 @@ function createParticle() {
   particle.position = new p5.Vector(random(width), random(height));
   particle.velocity = new p5.Vector(0,0);
   particle.acceleration = new p5.Vector(0,0);
+  particle.previousposition = particle.position.copy();
+  particle.colour = hexToRGB(random(palette), random(50,120));
   
   particles.push(particle);
 }
 
 function edgeCheck(particle) {  
-  if (particle.position.x < 0)
+  if (particle.position.x < 0) {
     particle.position.x = width;
-  else if (particle.position.x > width)
+    particle.previousposition = particle.position.copy();
+  }
+  else if (particle.position.x > width) {
     particle.position.x = 0;
+    particle.previousposition = particle.position.copy();
+  }
+    
   
-  if (particle.position.y < 0)
+  if (particle.position.y < 0) {
     particle.position.y = height;
-  else if (particle.position.y > height)
+    particle.previousposition = particle.position.copy();
+  }
+  else if (particle.position.y > height) {
     particle.position.y = 0;
-  
+    particle.previousposition = particle.position.copy();
+  }
+}
+
+function hexToRGB(hexColour, alpha) {
+  var aRgbHex = hexColour.replace("#","").match(/.{1,2}/g);
+  var aRgb = [
+            parseInt(aRgbHex[0], 16),
+            parseInt(aRgbHex[1], 16),
+            parseInt(aRgbHex[2], 16)
+        ];
+  let colour = color(aRgb[0], aRgb[1], aRgb[2], alpha);
+  return colour;
 }
